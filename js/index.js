99bd7e64a8e11cd836aad103c760542979d5d23a
rakibullahsazib <rakibullahsazib@yahoo.com>
2018-04-08 23:59:39 +0600

    Restruture code (mainly CSS) for mobile

--- js/index.js
+++ js/index.js
@@ -8,31 +8,31 @@ console.log("JS file is working");
   console.log("document is ready");
 });*/
 
-$(document).ready(function(){
-  if ($(window).width() < 320) {
-    $("body").css("background-size", "auto");
-  }else{$("body").css("background-size", "cover");}
-});
+// $(document).ready(function(){
+//   if ($(window).width() < 320) {
+//     $("body").css("background-size", "auto");
+//   }else{$("body").css("background-size", "cover");}
+// });
 
-$(window).resize(function() {
+// $(window).resize(function() {
 
-  if ($(window).width() < 480) {
-    $("body").css("background-size", "auto");
-  }else{$("body").css("background-size", "cover");}
+//   if ($(window).width() < 480) {
+//     $("body").css("background-size", "auto");
+//   }else{$("body").css("background-size", "cover");}
 
-  var turn1 = document.getElementById('turn1');
-  turn1.style.position = "absolute";
-  turn1.style.top = $("#btn1").position().top + 20 + 'px';
-  if (player1Name === "You") {
-    turn1.style.top = $("#btn4").position().top + 'px';
-  }
-  turn1.style.left = $("#playing-field").position().left + 'px';
+//   var turn1 = document.getElementById('turn1');
+//   turn1.style.position = "absolute";
+//   turn1.style.top = $("#btn1").position().top + 20 + 'px';
+//   if (player1Name === "You") {
+//     turn1.style.top = $("#btn4").position().top + 'px';
+//   }
+//   turn1.style.left = $("#playing-field").position().left + 'px';
 
-  var turn2 = document.getElementById('turn2');
-  turn2.style.position = "absolute";
-  turn2.style.top = $("#btn3").position().top + 'px';
-  turn2.style.left = $("#btn3").position().left + 80 + 'px';
-});
+//   var turn2 = document.getElementById('turn2');
+//   turn2.style.position = "absolute";
+//   turn2.style.top = $("#btn3").position().top + 'px';
+//   turn2.style.left = $("#btn3").position().left + 80 + 'px';
+// });
 
 var playerNum, sign1, sign2, player1, player2, btn1, btn2, btn3, btn4, btn5, btn6, btn7, btn8, btn9, step;
 var btn1Player, btn2Player, btn3Player, btn4Player, btn5Player, btn6Player, btn7Player, btn8Player, btn9Player;
